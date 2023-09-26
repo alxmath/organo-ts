b6798a323fa174f36a7171d53ef7c45b108781db
Alex Santos <alxmath@gmail.com>
2023-09-26 10:53:47 -0300

    Mergulhos mais fundos

--- src/componentes/CampoTexto/index.tsx
+++ src/componentes/CampoTexto/index.tsx
@@ -1,29 +1,40 @@
-import './CampoTexto.css'
+import "./CampoTexto.css";
 
-interface CampoTextoProps{
-    aoAlterado: (valor: string) => void
-    placeholder: string
-    label: string
-    valor: string
-    obrigatorio?: boolean
+interface CampoTextoProps {
+  aoAlterado: (valor: string) => void;
+  placeholder: string;
+  label: string;
+  valor: string;
+  obrigatorio?: boolean;
+  tipo?: "text" | "password" | "date" | "email" | "number";
 }
 
-const CampoTexto = ({aoAlterado, label, placeholder, valor, obrigatorio = false}: CampoTextoProps) => {
+const CampoTexto = ({
+  aoAlterado,
+  label,
+  placeholder,
+  valor,
+  obrigatorio = false,
+  tipo = "text",
+}: CampoTextoProps) => {
+  const placeholderModificada = `${placeholder}...`;
 
-    const placeholderModificada = `${placeholder}...` 
+  const aoDigitado = (evento: React.ChangeEvent<HTMLInputElement>) => {
+    aoAlterado(evento.target.value);
+  };
 
-    const aoDigitado = (evento: React.ChangeEvent<HTMLInputElement>) => {
-        aoAlterado(evento.target.value)
-    }
+  return (
+    <div className="campo-texto">
+      <label>{label}</label>
+      <input
+        value={valor}
+        onChange={aoDigitado}
+        required={obrigatorio}
+        placeholder={placeholderModificada}
+        type={tipo}
+      />
+    </div>
+  );
+};
 
-    return (
-        <div className="campo-texto">
-            <label>
-                {label}
-            </label>
-            <input value={valor} onChange={aoDigitado} required={obrigatorio} placeholder={placeholderModificada}/>
-        </div>
-    )
-}
-
-export default CampoTexto
+export default CampoTexto;
